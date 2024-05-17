fd1fa0fb22c3492f0df0b06057f59c1652b7a473
mcfaep <167646411+mcfaep@users.noreply.github.com>
2024-05-18 00:23:24 +0500

    fixed

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://backend.nomoredomainswork.ru';
+export const BASE_URL = 'https://backend.nomoredomainswork.ru/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
